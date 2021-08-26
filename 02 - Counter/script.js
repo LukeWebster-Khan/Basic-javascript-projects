150345dcce5c23b7ed14e917ad906747258ad759
Luke <lukewebsterkhan@gmail.com>
2021-08-26 22:02:31 +0100

    added event listener with if statement

--- 02 - Counter/script.js
+++ 02 - Counter/script.js
@@ -2,6 +2,17 @@ let count = 0;
 
 // value and buttons
 const value = document.querySelector("#value");
-const btns = document.querySelector(".btn");
+const btns = document.querySelectorAll(".btn");
 
 console.log(btns);
+//comment
+
+btns.forEach(function(btn) {
+     btn.addEventListener('click', function(e){
+        const styles = e.currentTarget.classList;
+        if(styles.contains('dec')){
+            count--;
+        }
+        value.textContent = count;
+     })
+});
