3946cc72e89f2647e38bdc023d963515ba920a6c
Luke <lukewebsterkhan@gmail.com>
2021-08-28 23:30:29 +0100

    added query selectors for buttons

--- 03 - Reviews/script.js
+++ 03 - Reviews/script.js
@@ -35,18 +35,47 @@ const author = document.getElementById("author");
 const job = document.getElementById("job");
 const info = document.getElementById("info");
 
-const prevBtn = document.querySelector("pre-btn");
-const nextBtn = document.querySelector("next-btn");
-const random = document.querySelector("random-btn");
+const prevBtn = document.querySelector(".pre-btn");
+const nextBtn = document.querySelector(".next-btn");
+const random = document.querySelector(".random-btn");
 
 // set starting review
-let currentItem = 3;
+let currentItem = 0;
 
 // load initial item
 window.addEventListener("DOMContentLoaded", function () {
+  showPerson();
+});
+
+// show person based on item
+function showPerson() {
   const item = reviews[currentItem];
   img.src = item.img;
   author.textContent = item.name;
   job.textContent = item.job;
   info.textContent = item.text;
+}
+
+// show next person
+
+nextBtn.addEventListener("click", function () {
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  showPerson();
+});
+
+// decrement person
+prevBtn.addEventListener("click", function () {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson();
+});
+
+random.addEventListener("click", function () {
+  currentItem = Math.floor(Math.random() * 4);
+  showPerson();
 });
