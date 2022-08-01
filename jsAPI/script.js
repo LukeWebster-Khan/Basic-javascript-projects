a14327833bb71521860dcc5c4a555406511e30e9
LukeWebster-Khan <Luke.Webster-Khan@theaccesspoint.co.uk>
2022-08-01 23:59:33 +0100

    added response check if statement

--- jsAPI/script.js
+++ jsAPI/script.js
@@ -1,5 +1,11 @@
 console.log(
-  fetch("https://api.github.com/users/LukeWebster-Khan").then((response) =>
-    response.json().then((data) => console.log(data))
-  )
+  fetch("https://api.github.com/users/LukeWebster-Khan").then((response) => {
+    if (response.ok) {
+      console.log("Success");
+    } else {
+      console.log("failure");
+    }
+    response.json().then((data) => console.log(data));
+  })
 );
+console.log(data.login);
