717a16eb4587e94e04aea48886ff1bd876c176cb
LukeWebster-Khan <Luke.Webster-Khan@theaccesspoint.co.uk>
2022-08-02 23:59:33 +0100

    refactored hrefs and added .then

--- jsAPI/script.js
+++ jsAPI/script.js
@@ -1,19 +1,23 @@
+let obj;
+
 function getData() {
-  fetch("https://api.github.com/users/LukeWebster-Khan").then((response) => {
-    if (response.ok) {
-      console.log("Success");
-    } else {
-      console.log("failure");
-    }
-    response.json().then((data) => console.log(data));
-    let userData = data;
-    return data;
-  });
+  fetch("https://api.github.com/users/LukeWebster-Khan")
+    .then((response) => {
+      if (response.ok) {
+        console.log("Success");
+      } else {
+        console.log("failure");
+      }
+      response.json().then((data) => (obj = data));
+
+      return userData;
+    })
+    .then(() => console.log(obj));
 }
-let data = getData();
-console.log(data);
+console.log(obj);
 
 const body = document.body;
+console.log(body);
 const h1 = document.createElement("h1");
-h1.innerText = "Test Header";
+h1.innerText = `${1 + 1}`;
 body.append(h1);
