4b1c39516a40d59a42b415839c5197d36b146694
LukeWebster-Khan <Luke.Webster-Khan@theaccesspoint.co.uk>
2022-08-04 22:08:56 +0100

    rendered js in html with then

--- jsAPI/script.js
+++ jsAPI/script.js
@@ -1,14 +1,23 @@
-let obj;
-fetch("https://api.github.com/users/LukeWebster-Khan")
-  .then((res) => res.json())
-  .then((data) => (obj = data))
-  .then(() => console.log(obj));
-console.log(obj);
-
-const body = document.body;
-console.log(body);
-const h1 = document.createElement("h1");
-h1.innerHTML = `<div>
- <h1>${obj}</h1>
-</div>`;
-body.append(h1);
+function fetchData() {
+  fetch("https://api.github.com/users/LukeWebster-Khan")
+    .then((res) => {
+      console.log(res);
+      if (!res.ok) {
+        throw Error("error");
+      }
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+      document.getElementById("app").innerHTML = data.name;
+    })
+    .catch((err) => console.log(err));
+}
+fetchData();
+// const body = document.body;
+// console.log(body);
+// const h1 = document.createElement("h1");
+// h1.innerHTML = `<div>
+//  <h1>${obj}</h1>
+// </div>`;
+// body.append(h1);
